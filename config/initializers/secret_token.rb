404b95092c8c3631dc9430a751268c83071a32c4
Harry Rickards <hrickards@gmail.com>
2013-07-06 13:48:13 +0100

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cal2::Application.config.secret_key_base = 'ff14e079b8e09c9ca0a7366275c29bed9e13f923432833e5500e3d8b51c82704fa06e3a58813c03c0456d626c6aa19e20a63994844d11622782fb7caa220bbb6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cal2::Application.config.session_store :cookie_store, key: '_cal2_session'
